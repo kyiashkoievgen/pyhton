e6207cb8e59d7b11c58f4d758428e44b5d93e2f6
kyiashkoievgen <91334393+kyiashkoievgen@users.noreply.github.com>
2021-10-16 21:08:20 +0300

    Delete lession5 directory

--- lession5/task_5_3.py
+++
@@ -1,9 +0,0 @@
-my_file = open("m_file3.txt", "r")
-while 1:
-    line = my_file.readline()
-    if line == '':
-        break
-    worker = line.split(" ")
-    if float(worker[1][:-1]) < 20000:
-        print(worker[0])
-my_file.close()
